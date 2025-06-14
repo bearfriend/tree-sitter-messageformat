19898aabdd16fe6813a16ed95248eeb0bf8aa6bd
Danny Gleckler <daniel.gleckler@gmail.com>
2025-06-14 10:59:29 -0400

    Fix node

--- bindings/node/index.js
+++ bindings/node/index.js
@@ -1,10 +1,13 @@
 const root = require("path").join(__dirname, "..", "..");
 
-module.exports =
+const binding =
   typeof process.versions.bun === "string"
     // Support `bun build --compile` by being statically analyzable enough to find the .node file at build-time
     ? require(`../../prebuilds/${process.platform}-${process.arch}/tree-sitter-messageformat.node`)
     : require("node-gyp-build")(root);
+
+// Export the language object directly
+module.exports = binding.language();
 
 try {
   module.exports.nodeTypeInfo = require("../../src/node-types.json");
